d06012b755ce750d197762aec2b79a8e261d1e47
Calvin <kwoncalvin43@gmail.com>
2023-04-14 16:13:53 -0700

    backend

--- backend/db/migrations/20230414215722-create-user.js
+++ backend/db/migrations/20230414215722-create-user.js
@@ -49,7 +49,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = "Users";
