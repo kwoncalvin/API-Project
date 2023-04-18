1ec6ed3295337dbfe282bff59d267870562146f6
Calvin <kwoncalvin43@gmail.com>
2023-04-18 10:11:52 -0700

    groups

---
+++ backend/db/migrations/20230418154122-create-group.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      organizerId: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      about: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      type: {
+        type: Sequelize.ENUM('In person', 'Online'),
+        allowNull: false
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Groups';
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/seeders/20230418161203-groups.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    options.tableName = 'Groups';
+    return queryInterface.bulkInsert(options, [
+      {
+        name: 'Group1',
+        about: 'about group 1',
+        type: 'Online',
+        private: 'false',
+        city: 'City1',
+        State: 'State1'
+      },
+      {
+        name: 'Group2',
+        about: 'about group 2',
+        type: 'In Person',
+        private: 'true',
+        city: 'City2',
+        State: 'State2'
+      },
+      {
+        name: 'Group3',
+        about: 'about group 3',
+        type: 'Online',
+        private: 'true',
+        city: 'City3',
+        State: 'State3'
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Groups';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['Group1', 'Group2', 'Group3'] }
+    }, {});
+  }
+};
