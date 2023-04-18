3a7eefac285036ae789af02c44b7a6cf4d59ea13
Calvin <kwoncalvin43@gmail.com>
2023-04-18 10:46:58 -0700

    p

--- backend/db/migrations/20230418154122-create-group.js
+++ backend/db/migrations/20230418154122-create-group.js
@@ -25,7 +25,7 @@ module.exports = {
         allowNull: false
       },
       type: {
-        type: Sequelize.ENUM('In person', 'Online'),
+        type: Sequelize.ENUM('In Person', 'Online'),
         allowNull: false
       },
       private: {
