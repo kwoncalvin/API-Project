5c9a79b08ea74c2ba992b92781d4845249dfd845
Calvin <kwoncalvin43@gmail.com>
2023-04-20 11:32:06 -0700

    /

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -71,6 +71,8 @@ router.delete(
 
 router.get(
     '/',
+    requireAuth,
+    restoreUser,
     (req, res) => {
       const { user } = req;
       if (user) {
