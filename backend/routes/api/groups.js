9d32d881f4fc621cb1fadfd4eab88276c666dc47
Calvin <kwoncalvin43@gmail.com>
2023-04-21 00:43:48 -0700

    create

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -4,9 +4,36 @@ const { Op } = require('sequelize');
 
 const { Group, Membership, GroupImage, User, Venue } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 
 const router = express.Router();
+
+const validateGroup = [
+    check('name')
+      .exists({ checkFalsy: true })
+      .isLength({ max: 60 })
+      .withMessage('Name must be 60 characters or less'),
+    check('about')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 50 })
+      .withMessage('About must be 50 characters or more'),
+    check('type')
+      .isIn(['Online', 'In person'])
+      .withMessage("Type must be 'Online' or 'In person'"),
+    check('private')
+      .exists({ checkFalsy: true })
+      .isBoolean()
+      .withMessage("Private must be a boolean"),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage("City is required"),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage("State is required"),
+    handleValidationErrors
+];
 
 router.get('/', async (req, res, next) => {
     const groups = await Group.findAll({
@@ -100,6 +127,22 @@ router.get('/:groupId', async (req, res, next) => {
         return next(err);
     }
     res.status(200).json(group);
+})
+
+router.post('/', requireAuth, validateGroup,
+    async (req, res, next) => {
+        const {name, about, type, private, city, state } = req.body;
+        const group = await Group.create({
+            organizerId: req.user.id,
+            name,
+            about,
+            type,
+            private,
+            city,
+            state
+        });
+        return res.json(group)
+
 })
 
 module.exports = router;
